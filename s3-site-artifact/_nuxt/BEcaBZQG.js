236d2b0ad452d2a0a49a2ce35ccbfdd0fa36d957
Ciao Chung <ciao0958@gmail.com>
2024-10-28 22:05:48 +0800

    chore: update artifact

---
+++ s3-site-artifact/_nuxt/BEcaBZQG.js
@@ -0,0 +1,1 @@
+import{c as o}from"./DzQh5E1e.js";import{k as t}from"./DZPsGlB4.js";import"./BduLK7P_.js";const m=t({__name:"404",setup(e){throw o({statusCode:404,message:"Page not found",fatal:!0})}});export{m as default};
---
+++ s3-site-artifact/_nuxt/MXT5-xEu.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DzQh5E1e.js";import{X as o,Q as r}from"./DZPsGlB4.js";import"./BduLK7P_.js";const t={};function c(n,m){return r(),o("div",null," home ")}const f=e(t,[["render",c]]);export{f as default};
